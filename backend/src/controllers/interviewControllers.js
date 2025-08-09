6febd5993be1c7bedafdee418eb36dac2245f57e
Bhagavan11 <bhagavan9182@gmail.com>
2025-08-09 12:52:08 +0530

    fixed 500 err  url

--- backend/src/controllers/interviewControllers.js
+++ backend/src/controllers/interviewControllers.js
@@ -1,5 +1,5 @@
 import multer from 'multer';
-// import pdfParse from 'pdf-parse';
+import pdfParse from 'pdf-parse';
 import dotenv from "dotenv";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,9 @@ import interviewRoutes from "./routes/interviewRoutes.js";
 dotenv.config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
+
 app.use(cookieParser());
 
 const corsOptions = {
